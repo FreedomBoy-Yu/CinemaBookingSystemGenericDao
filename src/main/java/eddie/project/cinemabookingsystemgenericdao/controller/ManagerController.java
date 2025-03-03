e2c3eb7c60d345851ba7f43f3eb733a8940c9f07
FreedonBoy-Yu <eddie23546224@gmail.com>
2025-03-03 20:05:25 +0800

    Book v.4 新增了ManagerController的使用者訂單想關功能

--- src/main/java/eddie/project/cinemabookingsystemgenericdao/controller/ManagerController.java
+++ src/main/java/eddie/project/cinemabookingsystemgenericdao/controller/ManagerController.java
@@ -1,5 +1,6 @@
 package eddie.project.cinemabookingsystemgenericdao.controller;
 
+import eddie.project.cinemabookingsystemgenericdao.dto.book.OrderCount;
 import eddie.project.cinemabookingsystemgenericdao.dto.movie.MovieDTO;
 import eddie.project.cinemabookingsystemgenericdao.entity.Book;
 import eddie.project.cinemabookingsystemgenericdao.entity.User;
@@ -7,8 +8,10 @@ import eddie.project.cinemabookingsystemgenericdao.service.MovieService;
 import eddie.project.cinemabookingsystemgenericdao.service.UserService;
 import eddie.project.cinemabookingsystemgenericdao.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Date;
 import java.util.List;
 
 @RestController
@@ -56,10 +59,64 @@ public class ManagerController {
     }
     /***********************************************到這裡是movie相關設置的controller***************/
     /***********************************************從這裡****************************************/
-    @GetMapping("/book/findAllBook")
+
+
+
+    @GetMapping("/findAllBook")
     public List<Book> findAllBook() {
         return bookService.findAll();
     }
+
+    @GetMapping("/findbookbyuserid") // 從後台尋找使用者的訂單資訊
+    public List<Book> findBookByUserId(@RequestParam("id") Integer id) {
+        return bookService.findBookByUserId(id);
+    }
+
+    @GetMapping("/status") // 從訂單狀態來查詢電影訂單
+    public List<Book> findByPaidStatus(@RequestParam("status") Integer status) {
+        return bookService.findByPaidStatus(status);
+    }
+
+    @GetMapping("/daterange") // 從訂單時間來篩選所有訂單
+    public List<Book> findBooksByDateRange(
+            @RequestParam("startDate") @DateTimeFormat(pattern = "yyyy-MM-dd") Date startDate,
+            @RequestParam("endDate") @DateTimeFormat(pattern = "yyyy-MM-dd") Date endDate
+    ) {
+        return bookService.findBooksByDateRange(startDate, endDate);
+    }
+
+    @GetMapping("/movieId") // 從電影 id 查詢訂單數量
+    public List<Book> findBooksByMovieId(@RequestParam("movieid") Integer movieid) {
+        return bookService.findBooksByMovieId(movieid);
+    }
+
+    @GetMapping("/paidcount") // 查詢並由大到小排序訂單與付款的資料，根據傳入值撈出有付款或尚未付款的資料
+    public List<OrderCount> findBookPaidCountByUser(@RequestParam("paid") Integer paid) {
+        return bookService.findBookPaidCountByUser(paid);
+    }
+
+    @GetMapping("/movieorder") // 從電影排序訂單數量，只顯示數量不顯示其他資訊
+    public List<OrderCount> findMovieOrderCount() {
+        return bookService.findMovieOrderCount();
+    }
+
+    @GetMapping("/orderpaidcount") // 用電影的 id 排序已付款的訂單
+    public List<OrderCount> findMovieOrderPaidCount(@RequestParam("paid") Integer paid) {
+        return bookService.findMovieOrderPaidCount(paid);
+    }
+
+    @GetMapping("/paiddaterange") // 用電影的 id 排序已付款的訂單，並設置排序的時間日期
+    public List<OrderCount> findMovieOrderPaidCountTimeRange(
+            @RequestParam("paid") Integer paid,
+            @RequestParam("startDate")@DateTimeFormat(pattern = "yyyy-MM-dd") Date startDate,
+            @RequestParam("endDate") @DateTimeFormat(pattern = "yyyy-MM-dd") Date endDate
+    ) {
+        return bookService.findMovieOrderPaidCountTimeRange(paid, startDate, endDate);
+    }
+
+
+
+
 
 
     /***********************************************到這裡是Booking相關的設置的controller***********/
